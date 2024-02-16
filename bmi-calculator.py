8c5a0adeeaab174618589d83421463d8e0b68271
Sleeptraphero <bastian.radlmaier@code.berlin>
2024-02-16 10:22:14 +0100

    change language to english

--- bmi-calculator.py
+++ bmi-calculator.py
@@ -1,36 +1,36 @@
 # BMI Calculator
-# BMI-Formel = Körpergewicht (in kg) geteilt durch Körpergröße (in m) zum Quadrat
+# BMI Formula = Body weight (in kg) divided by height (in m) squared
 
-# BMI < 18,5 	        Untergewicht
-# BMI 18,5 – 24,9 	    Normalgewicht
-# BMI 25 – 29,9 kg/m2 	Übergewicht
-# BMI 30 – 34,9 kg/m2 	Adipositas Grad I
-# BMI 35 – 39,9 kg/m2 	Adipositas Grad II
-# BMI ≥ 40 kg/m2 	    Adipositas Grad III 
+# BMI < 18.5 	    Underweight
+# BMI 18.5 – 24.9 	Normal weight
+# BMI 25 – 29.9   	Overweight
+# BMI 30 – 34.9   	Obesity Grade I
+# BMI 35 – 39.9   	Obesity Grade II
+# BMI ≥ 40        	Obesity Grade III
 
-weight = float(input("Bitte gib dein Gewicht in kg ein: "))
-height = float(input("Bitte gib deine Größe in cm ein: "))
-name = input("Bitte gib deinen Namen an: ")
+weight = float(input("Please enter your weight in kg: "))
+height = float(input("Please enter your height in cm: "))
+name = input("Please enter your name: ")
 
 def calculate_bmi(weight_kg, height_cm):
     BMI = ((weight_kg/(height_cm/100)**2))
     
-    print("Hallo " + name + "!\nDein BMI ist: " + str(round(BMI, 2)))
+    print("Hello " + name + "!\nYour BMI is: " + str(round(BMI, 2)))
 
     if BMI >0:
         if(BMI < 18.5):
-            print("Du hast untergewicht.")
+            print("You are underweight.")
         elif(BMI < 24.9):
-            print("Du hast Normalgewicht.")
+            print("You have normal weight.")
         elif(BMI < 29.9):
-            print("Du hast übergewicht.")    
+            print("You are overweight.")    
         elif(BMI < 34.9):
-            print("Du hast Adipositas Grad I")
+            print("You have Obesity Grade I")
         elif(BMI < 39.9):
-            print("Du hast Adipositas Grad II")
+            print("You have Obesity Grade II")
         elif(BMI > 40):
-            print("Du hast Adipositas Grad III.")
+            print("You have Obesity Grade III.")
         else:
-            print("Bitte gib gültige Werte an")
+            print("Please provide valid values.")
 
 calculate_bmi(weight, height)
